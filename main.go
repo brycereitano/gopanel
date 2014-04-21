1e0aa2b059879609b96fe03f0430771c50970376
Bryce Reitano <ZettaZete@gmail.com>
2014-04-20 23:47:11 -0600

    Made it work to replace my bash script.

--- main.go
+++ main.go
@@ -10,12 +10,31 @@ import (
 	"time"
 )
 
+var COLORS = map[string]string {
+	"{{ColorFg}}"									:"#FFdddddd",	"{{ColorBg}}"									:"#FF101010",
+	"{{ColorFocusedOccupiedFg}}"	:"#FFdddddd",	"{{ColorFocusedOccupiedBg}}"	:"#FF404040",
+	"{{ColorFocusedFreeFg}}"			:"#FFdddddd",	"{{ColorFocusedFreeBg}}"			:"#FF101010",
+	"{{ColorFocusedUrgentFg}}"		:"#FFdddddd",	"{{ColorFocusedUrgentBg}}"		:"#FFe84f4f",
+	"{{ColorOccupiedFg}}"					:"#FFdddddd",	"{{ColorOccupiedBg}}"					:"#FF404040",
+	"{{ColorFreeFg}}"							:"#FF555555",	"{{ColorFreeBg}}"							:"#FF101010",
+	"{{ColorUrgentFg}}"						:"#FFdddddd",	"{{ColorUrgentBg}}"						:"#FFd23d3d",
+	"{{ColorLayoutFg}}"						:"#FFdddddd",	"{{ColorLayoutBg}}"						:"#FF101010",
+	"{{ColorTitleFg}}"						:"#FF555555",	"{{ColorTitleBg}}"						:"#FF101010",
+	"{{ColorStatusFg}}"						:"#FFdddddd",	"{{ColorStatusBg}}"						:"#FF101010",
+}
+
+type Element struct {
+	name string
+	result string
+	format string
+}
+
 /*
  * Xtitle is used to pass the stream of data from xtitle -s into the ch string.
- * It passes all of its errors through the errCh 
+ * It passes all of its errors through the errCh
  * To invoke this function: go Xtitle(2000, ch, errCh)
 */
-func Xtitle(interval int64, ch chan string, errCh chan error) {
+func Xtitle(interval int64, ch chan Element, errCh chan error) {
 	buff := make([]byte, 1024)
 	cmd := exec.Command("xtitle", "-sf", "'%s'")
 
@@ -39,7 +58,8 @@ func Xtitle(interval int64, ch chan string, errCh chan error) {
 				return
 			}
 
-			ch <- string(buff)
+			out := "%{F{{ColorStatusFg}}B{{ColorStatusBg}}}" + strings.Fields(strings.TrimSpace(strings.TrimRight(string(buff), string(byte(0)))))[0]
+			ch <-Element{name:"xtitle",result:out}
 			buff = make([]byte, 1024)
 
 			time.Sleep(time.Duration(interval) * time.Millisecond)
@@ -49,10 +69,10 @@ func Xtitle(interval int64, ch chan string, errCh chan error) {
 
 /*
  * Load is used to pass the results of cat /proc/loadavg into the ch string every 'interval' milliseconds.
- * It passes all of its errors through the errCh 
+ * It passes all of its errors through the errCh
  * To invoke this function: go Load(2000, ch, errCh)
 */
-func Load(interval int, ch chan string, errCh chan error) {
+func Load(interval int, ch chan Element, errCh chan error) {
 	for {
 		out, err := ioutil.ReadFile("/proc/loadavg")
 		if err != nil {
@@ -61,8 +81,8 @@ func Load(interval int, ch chan string, errCh chan error) {
 		}
 
 		outStr := strings.Fields(string(out))
-		result := fmt.Sprintf("Load: %s %s %s\n", outStr[0], outStr[1], outStr[2])
-		ch <- result
+		result := "%{F{{ColorStatusFg}}B{{ColorStatusBg}}}" + fmt.Sprintf("Load: %s %s %s", outStr[0], outStr[1], outStr[2])
+		ch <-Element{name:"load",result:string(result)}
 
 		time.Sleep(time.Duration(interval) * time.Millisecond)
 	}
@@ -70,10 +90,10 @@ func Load(interval int, ch chan string, errCh chan error) {
 
 /*
  * Memory is used to pass the results of free -m into the ch string every 'interval' milliseconds.
- * It passes all of its errors through the errCh 
+ * It passes all of its errors through the errCh
  * To invoke this function: go Memory(2000, ch, errCh)
 */
-func Memory(interval int64, ch chan string, errCh chan error) {
+func Memory(interval int64, ch chan Element, errCh chan error) {
 	for {
 		out, err := exec.Command("free", "-m").Output()
 		if err != nil {
@@ -96,8 +116,8 @@ func Memory(interval int64, ch chan string, errCh chan error) {
 			return
 		}
 
-		result := fmt.Sprintf("Mem: %.2f/%.2fG\n", used/1024, total/1024)
-		ch <- result
+		result := "%{F{{ColorStatusFg}}B{{ColorStatusBg}}}" + fmt.Sprintf("Mem: %.2f/%.2fG", used/1024, total/1024)
+		ch <-Element{name:"memory",result:string(result)}
 
 		time.Sleep(time.Duration(interval) * time.Millisecond)
 	}
@@ -105,10 +125,10 @@ func Memory(interval int64, ch chan string, errCh chan error) {
 
 /*
  * Volume is used to pass the processed results of pacmd dump into the ch string every 'interval' milliseconds.
- * It passes all of its errors through the errCh 
+ * It passes all of its errors through the errCh
  * To invoke this function: go Volume(2000, ch, errCh)
 */
-func Volume(interval int64, ch chan string, errCh chan error) {
+func Volume(interval int64, ch chan Element, errCh chan error) {
 	for {
 		out, err := exec.Command("pacmd", "dump").Output()
 		if err != nil {
@@ -129,29 +149,31 @@ func Volume(interval int64, ch chan string, errCh chan error) {
 			}
 		}
 
-		ch <- fmt.Sprintf("Vol: %d%%\n", int(float64(desiredInt)/655.36))
+		result :="%{F{{ColorStatusFg}}B{{ColorStatusBg}}}" +  fmt.Sprintf("Vol: %d%%", int(float64(desiredInt)/655.36))
+		ch <-Element{name:"volume",result:result}
 		time.Sleep(time.Duration(interval) * time.Millisecond)
 	}
 }
 
 /*
  * Time is used to pass the formatted results of time.Now() into the ch channel string every 'interval' milliseconds.
- * It passes all of its errors through the errCh 
+ * It passes all of its errors through the errCh
  * To invoke this function: go Time(2000, ch, errCh)
 */
-func Time(interval int, ch chan string, errCh chan error) {
+func Time(interval int, ch chan Element, errCh chan error) {
 	for {
-		ch <- time.Now().Format("Mon, Jan 2, 2006 15:04:05\n")
+		result :="%{F{{ColorStatusFg}}B{{ColorStatusBg}}}" +  time.Now().Format("Mon, Jan 2, 2006 15:04:05")
+		ch <-Element{name:"time",result:string(result)}
 		time.Sleep(time.Duration(interval) * time.Millisecond)
 	}
 }
 
 /*
  * Temp is used to pass the contents of the path specified into the ch channel string every 'interval' milliseconds.
- * It passes all of its errors through the errCh 
+ * It passes all of its errors through the errCh
  * To invoke this function: go Temp(2000, ch, errCh)
 */
-func Temp(interval int, ch chan string, errCh chan error) {
+func Temp(interval int, ch chan Element, errCh chan error) {
 	for {
 		out, err := ioutil.ReadFile("/sys/bus/platform/devices/coretemp.0/temp1_input")
 		if err != nil {
@@ -164,18 +186,18 @@ func Temp(interval int, ch chan string, errCh chan error) {
 			errCh <- err
 			return
 		}
-
-		ch <- fmt.Sprintf("Temp: %dC\n", temp)
+		result := "%{F{{ColorStatusFg}}B{{ColorStatusBg}}}" + fmt.Sprintf("Temp: %dC", temp)
+		ch <-Element{name:"temp",result:result}
 		time.Sleep(time.Duration(interval) * time.Millisecond)
 	}
 }
 
 /*
- * StatusWm is used to pass the contents of the path specified into the ch channel string every 'interval' milliseconds.
- * It passes all of its errors through the errCh 
- * To invoke this function: go Temp(2000, ch, errCh)
+ * StatusWm takes the stream of information from bspc control --subscribe and pass it to  ch channel string every 'interval' milliseconds.
+ * It passes all of its errors through the errCh
+ * To invoke this function: go StatusWm(2000, ch, errCh)
 */
-func StatusWm(interval int64, ch chan string, errCh chan error) {
+func StatusWm(interval int64, ch chan Element, errCh chan error) {
 	buff := make([]byte, 1024)
 	cmd := exec.Command("bspc", "control", "--subscribe")
 
@@ -199,7 +221,28 @@ func StatusWm(interval int64, ch chan string, errCh chan error) {
 				return
 			}
 
-			ch <- string(buff)
+			temp := strings.TrimSpace(strings.TrimRight(string(buff), string(byte(0))))
+			tempSlice := strings.Split(temp, ":")
+			var out string
+			for _, str := range tempSlice {
+				switch str[0] {
+				case 'O':
+					out += "%{F{{ColorFocusedOccupiedFg}} U{{ColorFocusedOccupiedFg}} B{{ColorFocusedOccupiedBg}} +u} " + str[1:] + " %{-uB{{ColorBg}}}"
+				case 'F':
+					out += "%{F{{ColorFocusedFreeFg}} U{{ColorFocusedFreeFg}} B{{ColorFocusedFreeBg}} +u} " + str[1:] + " %{-uB{{ColorBg}}}"
+				case 'U':
+					out += "%{F{{ColorFocusedUrgentFg}} U{{ColorFocusedUrgentFg}} B{{ColorFocusedUrgentBg}} +u} " + str[1:] + " %{-uB{{ColorBg}}}"
+				case 'o':
+					out += "%{F{{ColorOccupiedFg}} B{{ColorOccupiedBg}}} " + str[1:] + " %{B{{ColorBg}}}"
+				case 'f':
+					out += "%{F{{ColorFreeFg}} B{{ColorFreeBg}}} " + str[1:] + " %{B{{ColorBg}}}"
+				case 'u':
+					out += "%{F{{ColorUrgentFg}} B{{ColorUrgentBg}}} " + str[1:] + " %{B{{ColorBg}}}"
+				case 'L':
+					out += "%{F{{ColorLayoutFg}} B{{ColorLayoutBg}}} " + strings.ToUpper(string(str[1])) + " %{B{{ColorBg}}}"
+				}
+			}
+			ch <- Element{name:"wm",result:out}
 			buff = make([]byte, 1024)
 
 			time.Sleep(time.Duration(interval) * time.Millisecond)
@@ -207,45 +250,45 @@ func StatusWm(interval int64, ch chan string, errCh chan error) {
 	}()
 }
 
+func DisplayInfo(format string) func(Element) {
+	var outString string
+	elements := make(map[string] Element)
+	return func(item Element) {
+		elements[item.name] = item
+		outString = format
+		for key, value := range elements {
+			outString = strings.Replace(outString, "{{"+key+"}}", value.result, -1)
+		}
+		for key, value := range COLORS {
+			outString = strings.Replace(outString, key,	value, -1)
+		}
+		fmt.Println(outString)
+	}
+}
+
 func main() {
+	ch := make(chan Element)
 	errCh := make(chan error)
 
-	xtitleCh := make(chan string)
-	loadCh := make(chan string)
-	memoryCh := make(chan string)
-	volumeCh := make(chan string)
-	timeCh := make(chan string)
-	tempCh := make(chan string)
-	statusWmCh := make(chan string)
+	go Load(10000, ch, errCh)
+	go Memory(10000, ch, errCh)
+	go Volume(5000, ch, errCh)
+	go Time(1000, ch, errCh)
+	go Temp(1000, ch, errCh)
+	go Xtitle(200, ch, errCh)
+	go StatusWm(200, ch, errCh)
 
-	go Load(10000, loadCh, errCh)
-	go Memory(10000, memoryCh, errCh)
-	go Volume(5000, volumeCh, errCh)
-	go Time(1000, timeCh, errCh)
-	go Temp(1000, tempCh, errCh)
-	go Xtitle(200, xtitleCh, errCh)
-	go StatusWm(200, statusWmCh, errCh)
-
+	format := fmt.Sprint("%{l}{{wm}} %{c}{{xtitle}} %{r}{{volume}} | {{memory}} | {{load}} | {{temp}} | {{time}}")
+	format = fmt.Sprint("%{S0}", format, "%{S1}", format, "%{S2}", format)
+	DisplayClosure := DisplayInfo(format)
 	for {
 		select {
 		case err := <-errCh:
 			log.Fatal(err)
 		default:
 			select {
-			case xtitle := <-xtitleCh:
-				fmt.Print(xtitle)
-			case load := <-loadCh:
-				fmt.Print(load)
-			case memory := <-memoryCh:
-				fmt.Print(memory)
-			case volume := <-volumeCh:
-				fmt.Print(volume)
-			case time := <-timeCh:
-				fmt.Print(time)
-			case temp := <-tempCh:
-				fmt.Print(temp)
-			case wm := <-statusWmCh:
-				fmt.Print(wm)
+			case item := <-ch:
+				DisplayClosure(item)
 			}
 		}
 	}
